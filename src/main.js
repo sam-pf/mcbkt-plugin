918c3266761ace39729ef13ab0a771b9c095ac19
Sam at PF <Sam@physicsfront.com>
2017-09-19 14:54:29 -0700

    ^: moved code from main.js to App.vue

--- src/main.js
+++ src/main.js
@@ -18,105 +18,12 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
-import iframe_phone from './js-ext/iframe-phone.js'
-import logdata_listener from './js-ext/iframe-logdata-listener.js'
-import post_logdata_for_mcbkt from './js-ext/mcbkt-client.js'
-
 Vue.config.productionTip = false
-
-function summarize_mcbkt_result (rv, mcbkt_ans, callback) {
-   const new_doc = rv.new_doc || {}
-   const cd = rv.cd || ""
-   const sep = ';'
-   let parts = [] // parts should not accept any sep containing string
-   const id = new_doc.id // challenge level
-   if (! id) return
-   parts.push ('L=' + id)
-   const scores = new_doc.iscores.join (',')
-   if (! scores) return
-   parts.push ('S=' + scores)
-   const cluster_long = new_doc.cluster_long
-   if (! cluster_long)
-      return callback ({formatStr: parts.join (sep)})
-   parts.push ('C=' + cluster_long)
-   parts.push ('T=' + new_doc.time)
-   let parvals = []
-   for (const name of ['M', 'pli', 'pt', 'pg', 'ps'])
-      try {
-         const v = Math.round (mcbkt_ans [name][0] * 100)
-         parvals.push ((isNaN (v) || v === undefined)? '?': v)
-      } catch (e) {
-         parvals = []
-         break
-      }
-   if (parvals.length)
-      parts.push ('M+=' + parvals.join (','))
-   try {
-      parts.push ('D=' + mcbkt_ans ['fit_duration'].toFixed (1))
-   } catch (e) {} // eslint-disable-line no-empty
-   callback ({formatStr: parts.join (sep)})
-   return cd
-}
 
 /* eslint-disable no-new */
 new Vue ({
    el : '#app',
    router,
    template : '<App/>',
-   components : { App },
-   props : {
-      application: {
-         type: String,
-         default: 'CODAP',
-      },
-      activity: {
-         type: String,
-         default: 'Ramp Game 2017 09',
-      },
-      logdata_listener_name: {
-         type: String,
-         default: 'Real Time MCBKT',
-      },
-   },
-   methods : {
-      cd_changed_cb: function (cd) {
-         this.ll.update_title ('RTMCBKT ' +  cd)
-      }
-   },
-   created : function () { // must be function, not =>
-      this.ll = new logdata_listener (
-         window.iframePhone ? window.iframePhone : iframe_phone,
-         (logdata, callback) => { // eslint-disable-line no-unused-vars
-            // console.log ("== main.js: posting logdata for mcbkt analysis: "
-            //              + JSON.stringify (logdata))
-            post_logdata_for_mcbkt (logdata, this.ll.get_state (false))
-            .then (
-               data => {
-                  // console.log ("== main.js: received data from UKDE: " +
-                  //              data)
-                  let d = JSON.parse (data)
-                  if (d.answer)
-                     if (this.mcbkt_fit_consumer) {
-                        summarize_mcbkt_result (
-                           this.mcbkt_fit_consumer (d), d.answer, callback)
-                        // if (cd)
-                        //   this.ll.update_title ('RT-MCBKT-' + cd)
-                     }
-               },
-               reason => {
-                  console.error ("** E: promise rejected.", reason)
-               }
-            )
-            .catch (
-               error => {
-                  console.error ("** E: error while processing promise.",
-                                 error)
-               }
-            )
-         },
-         this.logdata_listener_name,
-         {'application': this.application, 'activity': this.activity},
-         false,
-         0)
-   }
+   components : { App }
 })
