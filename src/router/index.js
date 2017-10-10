fcd6505c7c53dd2165a1a561b4ff21dfbd13a190
Sam at PF <Sam@physicsfront.com>
2017-10-10 10:34:21 -0700

    begin v-0.3.0: "?for=researcher" allowed

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Panel from '@/components/panel_for_student'
+import Panel_researcher from '@/components/panel_for_researcher'
 
 Vue.use (Router)
 
 export default new Router ({
   routes: [
+    {
+      path: '/',
+      query: { for: 'researcher'},
+      name: 'Panel',
+      component: Panel_researcher,
+      props: {
+         heading: '',
+         unit_activity_type: 'challenge'
+      }
+    },
     {
       path: '/',
       name: 'Panel',
