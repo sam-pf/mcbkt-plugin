ef414fa67a3f3dbd44673b01440f817457d2fc4f
Sam at PF <Sam@physicsfront.com>
2017-10-10 10:49:15 -0700

    ^: trying different router order

--- src/router/index.js
+++ src/router/index.js
@@ -9,8 +9,9 @@ export default new Router ({
   routes: [
     {
       path: '/',
+      query: { 'for': 'researcher'},
       name: 'Panel',
-      component: Panel,
+      component: Panel_researcher,
       props: {
          heading: '',
          unit_activity_type: 'challenge'
@@ -18,9 +19,8 @@ export default new Router ({
     },
     {
       path: '/',
-      query: { 'for': 'researcher'},
       name: 'Panel',
-      component: Panel_researcher,
+      component: Panel,
       props: {
          heading: '',
          unit_activity_type: 'challenge'
